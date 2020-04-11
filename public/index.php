17606a009be619d5ecd45355f9cc17fb156df2a9
Pierre Muller <pierremanu.muller@gmail.com>
2020-04-11 17:53:29 +0200

    Composer 2 quest

--- public/index.php
+++ public/index.php
@@ -4,3 +4,10 @@ require '../vendor/autoload.php';
 $hello = new \App\wcs\Hello();
 
 echo $hello->talk();
+
+// Composer Package management
+// New instance of SayHello from ehime package
+$helloCompo = new \HelloWorld\SayHello();
+
+// calling SayHello's method
+echo $helloCompo->world();
